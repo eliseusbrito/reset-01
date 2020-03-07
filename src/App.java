175182959000462f892226eeac2a9468614a02b2
unknown <eliseusbrito>
2020-03-07 12:13:12 -0300

    Removendo Mensagens Estranhas

--- src/App.java
+++ src/App.java
@@ -1,9 +1,5 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("outras coisas");
-        System.out.println("sem criatividade");
-        System.out.println("pra mensagens");
-        System.out.println("Agora eu sei usar o GitHub");
     }
 }
